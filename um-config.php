5186b677762607929f8d37b5ea0f3bd283c69b47
Charles Williams <cwilliams@oneupweb.com>
2015-02-20 14:12:04 -0500

    Do not spit out the password in plaintext

--- um-config.php
+++ um-config.php
@@ -412,7 +412,7 @@ $this->sections[] = array(
 										  '{login_url}'  . "\r\n\r\n" .
 										  'Your account e-mail: {email}' . "\r\n" .
 										  'Your account username: {username}' . "\r\n" .
-										  'Your account password: {password}' . "\r\n\r\n" .
+										  //'Your account password: {password}' . "\r\n\r\n" .
 										  'If you have any problems, please contact us at {admin_email}'  . "\r\n\r\n" .
 										  'Thanks,' . "\r\n" .
 										  '{site_name}',
@@ -512,7 +512,7 @@ $this->sections[] = array(
 										  '{login_url}'  . "\r\n\r\n" .
 										  'Your account e-mail: {email}' . "\r\n" .
 										  'Your account username: {username}' . "\r\n" .
-										  'Your account password: {password}' . "\r\n\r\n" .
+										  //'Your account password: {password}' . "\r\n\r\n" .
 										  'If you have any problems, please contact us at {admin_email}'  . "\r\n\r\n" .
 										  'Thanks,' . "\r\n" .
 										  '{site_name}',
